fbd7d9a48f2f48a767379eef9adc28dc68dd3414
Derik <62659313+makemepresident@users.noreply.github.com>
2020-10-01 12:15:06 -0700

    added gitignore

--- src/crib.py
+++ src/crib.py
@@ -4,5 +4,6 @@ from Card import Card
 hand1 = Card.list2hand([0,2,4,6])
 hand2 = Card.list2hand([1,3,5,7])
 
-g = GameHandler("Alice", "Bill", "Charlie")
+# g = GameHandler("Alice", "Bill", "Charlie")
+g = GameHandler("Derik", "Zak")
 g.gameLoop()
